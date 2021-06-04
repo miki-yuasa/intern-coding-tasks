f12e92d31886cf88bf93075c93cfa6b8c36494f6
Mikihisa Yuasa <mikihisa.yuasa@wisc.edu>
2021-06-03 22:03:46 -0500

    q3: naive implementation

--- 2020/src/q3.py
+++ 2020/src/q3.py
@@ -12,7 +12,7 @@ class SeatNeighbor:
         self.negative_distance = left_seat - right_seat
 
     def tuplify(self):
-        return((self.negative_distance,[self.left_seat, self.right_seat]))
+        return (self.negative_distance,[self.left_seat, self.right_seat])
 
 def main():
     from os import cpu_count
@@ -31,31 +31,53 @@ def main():
 
 def sum_even_seats(na_1: List[int]) -> str:
     import heapq
+    import numpy as np
+    import numpy.typing as npt
 
     n:int
     a_1:int
-    n,a_1 = na_1[0]
+    n, a_1 = na_1
 
-    seat_neighbors: List[int] = [SeatNeighbor(0,a_1).tuplify(), SeatNeighbor(a_1,n).tuplify()]
-    heapq.heapify(seat_neighbors)
+    #seat_neighbors: List[int] = [SeatNeighbor(1,a_1+1).tuplify(), SeatNeighbor(a_1,n+2).tuplify()]
+    #heapq.heapify(seat_neighbors)
+
+    seats: npt.ArrayLike = get_seats(n,a_1)
 
     seat_sum: int = 0
 
-    for i in range(n-1):
-        seat_num, seat_neighbors = get_seated(na_1, seat_neighbors)
-        if i % 2 == 1:
-            seat_sum += seat_num
+    for i in range(np.ceil(n/2).astype('int')-1):
+        new_seat_ind: int = seats.argmax() + 1
+        new_seats: npt.ArrayLike = get_seats(n, new_seat_ind)
+        seats: npt.ArrayLike = np.minimum(seats, new_seats)
 
+        #seat_num, seats = get_seated(na_1, seats)
+        if i % 2 == 0:
+            seat_sum += new_seat_ind
+
+    empty_seats_inds: npt.ArrayLike = np.argwhere(seats==1) + 1
+
+    if np.ceil(n/2)%2 == 0:
+        seat_sum += np.sum(empty_seats_inds[1::2])
+    else:
+        seat_sum += np.sum(empty_seats_inds[::2])
+    
     return '{:.0f}\n'.format(seat_sum)
+
+def get_seats(n: int, a_i:int):
+    import numpy as np
+    import numpy.typing as npt
+
+    added_seats: npt.ArrayLike = np.array([i for i in range(a_i-1, 0, -1)] + [i for i in range(n - (a_i - 1))])
+    return added_seats
 
 def get_seated(na_1:List[int], seat_neighbors:List[Tuple[int]]):
     import heapq
     import math
 
     farthest_seats = heapq.heappop(seat_neighbors)
-    seat_ind:int = math.floor(sum(seat_neighbors[1])/2)
-    heapq.heappush(seat_neighbors,SeatNeighbor(seat_neighbors[1][0], seat_ind).tuplify())
-    heapq.heappush(seat_neighbors,SeatNeighbor(seat_ind, seat_neighbors[1][1]).tuplify())
+    seat_ind:int = math.floor(sum(farthest_seats[1])/2)
+    heapq.heappush(seat_neighbors,SeatNeighbor(farthest_seats[1][0], seat_ind).tuplify())
+    heapq.heappush(seat_neighbors,SeatNeighbor(seat_ind, farthest_seats[1][1]).tuplify())
 
     return seat_ind, seat_neighbors
 
--- 2020/src/test_q3.py
+++ 2020/src/test_q3.py
@@ -15,7 +15,7 @@ class TestQ3(unittest.TestCase):
 
         for n, a_1, expected_result in test_patterns:
             with self.subTest(n=n, a_1=a_1):
-                self.assertEqual('{:.0f}\n'.format(expected_result), q3.get_seat([n,a_1]))
+                self.assertEqual('{:.0f}\n'.format(expected_result), q3.sum_even_seats([n,a_1]))
 
 if __name__ == '__main__':
     unittest.main()
